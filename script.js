a75c5e036dcd2124f4d74b101a77b4baee949dc4
pkk024 <63501552+pkk024@users.noreply.github.com>
2021-07-07 12:13:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,18 +9,37 @@ function myFunction(){
     var cell6=row.insertCell();
     var cell7=row.insertCell();
     var cell8=row.insertCell();
-
+    var txt="";
+    var checkBoxlength;
+    function check(){
+        var p1=document.forms[0];
+        
+        
+        for(var i=0;i<p1.length;i++)
+        if(p1[i].checked){
+checkBoxlength++;
+    txt=txt+p1[i].value+",";
+   
+    
+        }
+    }
+    
+    
+    check();
+   
+   
 cell1.innerHTML=document.getElementById("Firstname").value;
 cell2.innerHTML=document.getElementById("Lastname").value;
 cell3.innerHTML=document.getElementById("Address").value;
 cell4.innerHTML=document.getElementById("number").value;
 cell5.innerHTML=document.getElementById("Gender").value;
-cell5.innerHTML=document.getElementById("inlineCheckbox1").value;
 
 
+cell6.innerHTML=txt;
 cell7.innerHTML=document.getElementById("state").value;
 cell8.innerHTML=document.getElementById("country").value;
-  
+
+
 
 document.getElementById("myForm").reset();
 
